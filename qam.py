efb50fea6f5da1579a47065a1f951fcff2bcc022
dioday45 <65977803+dioday45@users.noreply.github.com>
2022-05-19 14:28:23 +0200

    Phase done

--- qam.py
+++ qam.py
@@ -6,6 +6,7 @@ class QAM:
     def __init__(self, m):
         self.m = m
         self.const, self.code = self.make_const_code(m)
+        self.K = 10
 
     def make_const_code(self, m):
         const = []
@@ -37,15 +38,22 @@ class QAM:
             tmp += byte
 
         sep = [tmp[i:i+N] for i in range(0, len(tmp), N)]
+
+        # Determine phase
+        for _ in range(self.K):
+            result.append(complex(1, 0))
+
         for byte in sep:
             result.append(self.const[int("".join(str(k) for k in byte), 2)])
         return np.array(result)
 
     def decode(self, s):
+        phase = s[0:self.K-1]
+        s = s[self.K: len(s)]
         tmp = ''
-        theta = -np.angle(s[0])
+        theta = -np.angle(sum(phase))
         for c in s:
-            shifted = abs(c)*np.exp(1j*(theta+np.angle(c)))
+            shifted = c*np.exp(1j*(theta))
             idx = np.argmin([abs(shifted-e) for e in self.const])
             tmp += self.code[idx]
         return [tmp[i:i+8] for i in range(0, len(tmp), 8)]
